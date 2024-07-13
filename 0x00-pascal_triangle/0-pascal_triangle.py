59ebd250bcd03c02658dd4c4ef4b7aaf8836a3fc
Adebayo Ifeoluwa <ifeoluwa.adebayo2003@gmail.com>
2024-07-13 05:40:41 +0000

    a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+ pascal_triangle 
+"""
+
+
+def fact(n):
+    """
+    Returns the factorial of n
+    """
+    if n < 0:
+        return None  # or raise an exception
+    elif n == 0:
+        return 1
+    else:
+        return n * fact(n - 1)
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            row.append(fact(i) // (fact(j) * fact(i - j)))
+        triangle.append(row)
+    return triangle
